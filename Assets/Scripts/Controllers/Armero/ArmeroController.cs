598dd46a366753600b4bf411f629557eb867ee57
Alex <alexalmendroal@gmail.com>
2021-03-12 14:17:33 +0100

    Armero dataDesign

---
+++ Assets/Scripts/Controllers/Armero/ArmeroController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmeroController : MonoBehaviour
+{
+    public enum TiposDeArmas { PISTOLA, ESCOPETA, METRALLETA, SNIPER, LANZACOHETES };
+    public enum TipoDeModificacion { FUEGO, AIRE, ROBO, ELECTRICIDAD, OSCURIDAD };
+
+    public struct Stats
+    {
+        public int daño;
+        public int velocidadDeRecarga;
+        public int cargador;
+        public int alcance;
+
+    }
+    public struct Arma {
+        public TiposDeArmas tipoDeArma;
+        public TipoDeModificacion tipoDeMod;
+        public bool equipado;
+        public bool bloqueado;
+        public Stats stats;
+
+    }
+    public Arma[] arma;
+
+
+    //GameObjects que se modificaran dependiendo de l informacion de arriba//
+    [SerializeField]
+    GameObject[] stats;
+    GameObject[] bloqued;
+    GameObject[] mods;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
